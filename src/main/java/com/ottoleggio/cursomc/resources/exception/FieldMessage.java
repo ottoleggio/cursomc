3d2677bf3dcb6f43cdf13e4293e70be711a31f7f
otto <ottoleggio@gmail.com>
2021-07-05 19:34:05 -0300

    retorno da validacao personalizada

---
+++ src/main/java/com/ottoleggio/cursomc/resources/exception/FieldMessage.java
@@ -0,0 +1,38 @@
+package com.ottoleggio.cursomc.resources.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String fieldName;
+	private String message;
+	
+	public FieldMessage() {
+	}
+
+	public FieldMessage(String filedName, String message) {
+		super();
+		this.fieldName = filedName;
+		this.message = message;
+	}
+
+	public String getFiledName() {
+		return fieldName;
+	}
+
+	public void setFiledName(String filedName) {
+		this.fieldName = filedName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+}
