af153a7bc70308f4c639aa04948c754b9ff64657
otto <ottoleggio@gmail.com>
2021-07-07 19:10:48 -0300

    correcoes

--- src/main/java/com/ottoleggio/cursomc/services/ClienteService.java
+++ src/main/java/com/ottoleggio/cursomc/services/ClienteService.java
@@ -42,7 +42,7 @@ public class ClienteService {
 			repo.deleteById(id);
 		}
 		catch(DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir um cliente que possui pedidos");
+			throw new DataIntegrityException("Não é possível excluir pois há entidades relacionadas");
 		}
 	}
 	
